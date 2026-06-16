5b89695e9e1cf41731b18e8ca99701b0e8c8547a
stchordjallian-sys <stchordjallian@edenschool.fr>
2026-06-16 11:50:07 +0200

    y

---
+++ app.js
@@ -0,0 +1,9 @@
+const boutons = document.querySelector("#coltrips");
+
+var balisedoc = document.getElementById("#trip-new");
+
+function myFunction() {
+	balisedoc[0].classList.add("trips-card2");
+
+	balisedoc[0].classList.remove("trips-card");
+}
